2a920297675a6b0f317f1387947ab02fb9eaad20
aquapi <aquaplmc@gmail.com>
2024-02-22 10:05:55 +0700

    Optimization: Only try assigning props to headers when headers are already set

--- src/core/utils/createSend.ts
+++ src/core/utils/createSend.ts
@@ -14,6 +14,6 @@ export default function createSend(headers: Record<string, string>, f: any): any
     // t is the input response init
     return Function(
         'h', 'i', 'f',
-        `return (b,t)=>{if(typeof t==='undefined')return new Response(${body},i);const{headers}=t;if(typeof headers==='undefined')t.headers=h;${propAssigns.join(';')};return new Response(${body},t)}`
+        `return (b,t)=>{if(typeof t==='undefined')return new Response(${body},i);const{headers}=t;if(typeof headers==='undefined')t.headers=h;else{${propAssigns.join(';')}}return new Response(${body},t)}`
     )(headers, { headers }, f);
 }
