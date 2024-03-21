8f1e6a4c3513225604f2434d299cb1c9c6e6e619
aquapi <aquaplmc@gmail.com>
2024-03-21 19:35:42 +0700

    Debugging

--- tests/bun/listen.ts
+++ tests/bun/listen.ts
@@ -1,9 +1,17 @@
 import { bit, type Byte } from '@bit-js/byte';
 
+function replacer(_: any, value: any) {
+    return typeof value === 'function' ? value.toString() : value;
+}
+
 /**
  * Serve the app using Bun and create a bit client
  */
 export default function listen<App extends Byte<any>>(app: App, port: number | string) {
     const server = Bun.serve({ fetch: app.fetch, port, development: false });
+
+    // Router debug
+    console.log(JSON.stringify(app.router, replacer, 4));
+
     return bit<App>(server.url.href);
 }
