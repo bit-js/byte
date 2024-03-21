53605955bb01e93d89dd9883d5184ae14530ba30
aquapi <aquaplmc@gmail.com>
2024-03-21 17:01:33 +0700

    Speed up query

--- src/core/server/utils/compile/send.ts
+++ src/core/server/utils/compile/send.ts
@@ -1,20 +1,12 @@
 export default function createSend(headers: Record<string, string>, bodyWrapper: any): any {
     // Response body wrapper
-    const body = bodyWrapper === null
-        ? 'b'
-        : typeof bodyWrapper === 'string'
-            ? `${bodyWrapper}(b)`
-            : 'f(b)';
+    const body = bodyWrapper === null ? 'b' : typeof bodyWrapper === 'string' ? `${bodyWrapper}(b)` : 'f(b)';
 
+    // Assign header props
     const propAssigns = [];
     for (const key in headers)
         propAssigns.push(`headers[${JSON.stringify(key)}]??=${JSON.stringify(headers[key])}`);
 
-    // h is default response headers
-    // i is default response init
-    // f is the body parser
-    // b is the input body
-    // t is the input response init
     return Function(
         'h', 'i', 'f',
         `return (b,t)=>{if(typeof t==='undefined')return new Response(${body},i);const{headers}=t;if(typeof headers==='undefined')t.headers=h;else{${propAssigns.join(';')}}return new Response(${body},t)}`
--- src/core/server/utils/query.ts
+++ src/core/server/utils/query.ts
@@ -21,14 +21,14 @@ export const query: {
         const search = JSON.stringify(encodeURIComponent(name) + '=');
         const searchLen = search.length - 2;
 
-        return $pass(Function(`return (c)=>{const {url}=c.req;const i=url.indexOf(${search},c.pathEnd+1)+${searchLen};if(i===${searchLen - 1})return null;const n=url.indexOf("&",i);return n===-1?url.substring(i):url.substring(i,n);}`)());
+        return $pass(Function(`return ({pathEnd,req:{url}})=>{const i=url.indexOf(${search},pathEnd+1)+${searchLen};if(i===${searchLen - 1})return null;const n=url.indexOf("&",i);return n===-1?url.substring(i):url.substring(i,n);}`)());
     },
 
     values: (name, maxValues) => {
         const search = JSON.stringify(encodeURIComponent(name) + '=');
         const searchLen = search.length - 2;
 
-        return $pass(Function(`return (c)=>{const {url}=c.req;const r=[];let i=url.indexOf(${search},c.pathEnd+1)+${searchLen};while(i!===${searchLen - 1}${typeof maxValues === 'number' ? `&&r.length<${maxValues}` : ''}){const n=url.indexOf("&",i);if(n===-1){r.push(url.substring(i));return r;}r.push(url.substring(i,n));i=url.indexOf(${search},n+1)}return r}`)());
+        return $pass(Function(`return ({pathEnd,req:{url}})=>{const r=[];let i=url.indexOf(${search},pathEnd+1)+${searchLen};while(i!===${searchLen - 1}${typeof maxValues === 'number' ? `&&r.length<${maxValues}` : ''}){const n=url.indexOf("&",i);if(n===-1){r.push(url.substring(i));return r;}r.push(url.substring(i,n));i=url.indexOf(${search},n+1)}return r}`)());
     },
 
     get: $pass((ctx) => {
