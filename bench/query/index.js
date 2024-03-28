e8159d77ed1ae9aca96ff8769923546c0a8f03d4
aquapi <aquaplmc@gmail.com>
2024-03-28 11:12:09 +0700

    Update bench

--- bench/query/index.js
+++ bench/query/index.js
@@ -1,26 +1,26 @@
 import { group, run, bench } from 'mitata';
 import { query, Context } from '../..';
 
-const params = 'id=1&name=Reve&age=16&admin';
+const params = 'items=1&items=2&name=Reve&age=16&admin';
 const ctx = new Context(new Request('http://localhost:3000/?' + params));
 
 const parse = query.schema({
-    id: 'number',
-    name: 'string',
-    age: 'number',
-    admin: 'bool'
+    items: { type: 'number', maxValues: 10 },
+    name: { type: 'string' },
+    age: { type: 'number' },
+    admin: { type: 'bool' }
 });
 console.log(parse.toString());
 
 group('Query parsing', () => {
     bench('Schema', () => {
         const o = parse(ctx);
-        return `${o.id} ${o.name} ${o.age} ${o.admin}`;
+        return `${o.items.join()} ${o.name} ${o.age} ${o.admin}`;
     });
 
     bench('URLSearchParams', () => {
         const params = new URLSearchParams(ctx.req.url.substring(ctx.pathEnd + 1));
-        return `${+params.get('id')} ${params.get('name')} ${+params.get('age')} ${params.has('admin')}`;
+        return `${params.getAll('items').join()} ${params.get('name')} ${+params.get('age')} ${params.has('admin')}`;
     });
 });
 
